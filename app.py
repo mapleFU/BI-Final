37239d924d01945ff77dfc14f8f8daf81d8d71a3
mapleFU <1506118561@qq.com>
2019-06-16 02:10:21 +0800

    [ADD] add basic rules for the program

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask.views import View
 from neo4j import GraphDatabase
 from py2neo import Graph, NodeMatcher, Database
 import py2neo
@@ -19,32 +20,61 @@ g = Graph(driver_address)
 app = Flask(__name__)
 
 
-def permlize_result(record: py2neo.Record):
-    for k, v in dict(record).items():
-        print(type(v.labels))
+def permlize_single_result(record_dict: dict):
+    for _, v in record_dict.items():
+        # print(type(v.labels))
         value_dict = dict(v)
         value_dict["id"] = value_dict['permID']
-        return value_dict
+        yield value_dict
+
+
+def permlize_result(record: py2neo.Record):
+    drecord = dict(record)
+    if len(drecord) <= 1:
+        yield permlize_single_result(drecord)
+    if len(drecord) == 3:
+        dic2 = dict()
+        for k in ('s', 'o'):
+            value_dict = record[k]
+            value_dict["id"] = value_dict['permID']
+            dic2[k] = value_dict["id"]
+            yield value_dict
+        value_dict = record['p']
+        value_dict["from"] = dic2['s']
+        value_dict["to"] = dic2['o']
+        yield value_dict
+
+
+flatten = lambda l: [item for sublist in l for item in sublist]
 
 
 def merge_result(result: py2neo.Cursor):
-    return [permlize_result(r) for r in result]
+    return flatten([permlize_result(r) for r in result])
 
 
-@app.route('/')
-def hello_world():
-    cql = "MATCH (p:Person)-[t:TenureInOrg]->(o:Organization) RETURN t LIMIT 20"
-    return jsonify(merge_result(g.run(cql)))
-
-
-@app.route('/nmsl')
-def load_all():
-    cql = '''
-        MATCH (p:Person)-[]-(f:Officership)-[]-(o:Organization)
-        return distinct o LIMIT 5
+@app.route('/person/<pid>/organization')
+def person_organization(pid):
+    """
+    根据PERSON 返回 ORGANIZATION
+    MATCH (p:Person{permID:$id1})-[r]-(i:Institution)
+    return distinct p, r, i
+    """
+    cql = f'''
+        MATCH (s:Person {{permID: '{pid}' }})-[p:isPositionIN]-(o:Organization)
+        return s, p, o
     '''
-
+    print(cql)
     return jsonify(merge_result(g.run(cql)))
+
+
+@app.route('/person/<pid>/institution')
+def person_institution(pid):
+    raise NotImplemented()
+
+
+@app.route('/organization/<pid>/organization')
+def organization_organization(pid):
+    raise NotImplemented()
 
 
 if __name__ == '__main__':
