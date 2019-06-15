e63026ea45986ee3c4ddf65aece498a341f6d601
Rebecca-yh <1652818@tongji.edu.cn>
2019-06-16 06:22:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,13 @@ def permlize_node_result(record_node: Node):
     value_dict["id"] = value_dict['permID']
     return value_dict
 
-
+#？？？？？？
 def permlize_relationship_result(record_node: Relationship):
     value_dict = dict(record_node)
     value_dict["from"] = dict(record_node.start_node)['permID']
-    value_dict["to"] = dict(record_node.start_node)['permID']
+    value_dict["to"] = dict(record_node.start_node)['permID'] #写错了？所有边都成环
     return value_dict
+#node,还需要添加类别(label)信息
 
 
 def permlize_result(record: py2neo.Record):
@@ -49,37 +50,99 @@ flatten = lambda l: [item for sublist in l for item in sublist]
 def merge_result(result: py2neo.Cursor):
     nodes, relations = list(), list()
     for record in result:
+        print(record)
         for n, r in permlize_result(record):
             relations.append(r) if n is None else nodes.append(n)
     return {
         'nodes': nodes,
         'relationships': relations
     }
+"""
+/organization/4296405163
+/person/34418264994
+/person/34418264994/organization/4296405163
+"""
 
 
-@app.route('/person/<pid>/organization')
-def person_organization(pid):
-    """
-    根据PERSON 返回 ORGANIZATION
-    MATCH (p:Person{permID:$id1})-[r]-(i:Institution)
-    return distinct p, r, i
-    """
+#1.个人-组织
+"""
+1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
+为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
+"""
+@app.route('/person/<pid>/organization/<oid>')
+def person_organization(pid,oid):
+
     cql = f'''
-        MATCH (s:Person {{permID: '{pid}' }})-[p:isPositionIN]-(o:Organization)
+        MATCH (s:Person {{permID: '{pid}' }})-[p:isPositionIN]-(o:Organization{{permID: '{oid}' }})
         return s, p, o
     '''
     print(cql)
     return jsonify(merge_result(g.run(cql)))
 
+#2.
+"""
+1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
+为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
+"""
+@app.route('/person/<pid1>/person/<pid2>')
+def person_person(pid1,pid2):
 
-@app.route('/person/<pid>/institution')
-def person_institution(pid):
-    raise NotImplemented()
+    cql = f'''
+        MATCH (s:Person {{permID: '{pid1}' }})-[p]-(o:Person{{permID: '{pid2}' }})
+        return s, p, o
+    '''
+    print(cql)
+    return jsonify(merge_result(g.run(cql)))
+#3.
+"""
+1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
+为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
+"""
+@app.route('/organization/<oid1>/organization/<oid2>')
+def organization_organization(oid1, oid2):
+    cql = f'''
+        MATCH (s:Organization {{permID: '{oid1}' }})-[p]-(o:Organization{{permID: '{oid2}' }})
+        return s, p, o
+    '''
+    print(cql)
+    return jsonify(merge_result(g.run(cql)))
 
+#4.
+"""
+2。输⼊入⼀一个实体（如Alibaba），查询其关联的所有关系和关联实体；
+"""
+@app.route('/person/<pid>')
+def person(pid):
+    cql=f'''MATCH (s:Person{{permID:'{pid}'}})-[p]-(o) return s, p, o '''
+    print(cql)
+    return jsonify(merge_result(g.run(cql)))
 
-@app.route('/organization/<pid>/organization')
-def organization_organization(pid):
-    raise NotImplemented()
+#5.
+"""
+2。输⼊入⼀一个实体（如Alibaba），查询其关联的所有关系和关联实体；
+"""
+@app.route('/organization/<oid>')
+def organization(oid):
+    cql=f'''MATCH (s:Organization{{permID:'{oid}'}})-[p]-(o) return  s, p, o'''
+    print(cql)
+    return jsonify(merge_result(g.run(cql)))
+
+#6.查看某个institution相关的person
+#不在要求内，非功能性需求
+@app.route('/institution/<iname>')
+def organization(iid):
+    cql=f'''MATCH (s:Institution{{name:'{iname}'}})-[p]-(o) return  s, p, o'''
+    print(cql)
+    return jsonify(merge_result(g.run(cql)))
+
+#7.查看某个industryGroup相关的organization
+#不在要求内，非功能性需求
+@app.route('/idustryGroup/<ilabel>')
+def organization(ilabel):
+    cql=f'''MATCH (s:IndustryGroup{{name:'{ilabel}'}})-[p]-(o) return  s, p, o'''
+    print(cql)
+    return jsonify(merge_result(g.run(cql)))
+
 
 
 if __name__ == '__main__':
