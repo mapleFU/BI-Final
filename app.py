d772275f5338c122870e8760d3501d135f2c28e8
mapleFU <1506118561@qq.com>
2019-06-16 13:32:34 +0800

    [Update] Fix duplicate of relationship

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from py2neo import Graph, NodeMatcher, Database
 import py2neo
 
 import os
+from typing import List, Dict
 
 driver_address = ''
 
@@ -21,7 +22,7 @@ g = Graph(driver_address)
 app = Flask(__name__)
 
 
-LABEL_ATTR_SET =set(["institution", "title", "organizationName", "givenName"])
+LABEL_ATTR_SET =set(["institution", "title", "organizationName", "givenName", "label"])
 
 
 def tag_label(value_dict: dict):
@@ -29,7 +30,7 @@ def tag_label(value_dict: dict):
         if k in LABEL_ATTR_SET:
             value_dict["label"] = value_dict[k]
             return
-    print("Warning, No label!")
+    print(f"Warning dict {value_dict}, No label!")
 
 
 def permlize_node_result(record_node: Node):
@@ -40,9 +41,16 @@ def permlize_node_result(record_node: Node):
 
 
 def permlize_relationship_result(record_node: Relationship):
+    node_type = 'Unknown'
+    try:
+        node_type = list(record_node.types())[0]
+    except _:
+        print('Exception occured with getting node_type')
     value_dict = dict(record_node)
+
     value_dict["from"] = dict(record_node.start_node)['permID']
     value_dict["to"] = dict(record_node.end_node)['permID']
+    value_dict["type"] = node_type
     return value_dict
 
 
@@ -55,19 +63,35 @@ def permlize_result(record: py2neo.Record):
             yield (None, permlize_relationship_result(v))
 
 
+def remove_duplicate_relationships(r: List[Dict])->List[Dict]:
+    # sort by from and to
+    r.sort(key=lambda x: (x['from'], x['to'], x['type']))
+    new_node = list()
+    last_node = None
+    equals = lambda a, b: a is not None and a['from'] == b['from'] \
+                           and a['to'] == b['to'] and a['type'] == b['type']
+    for item in r:
+        if not equals(last_node, item):
+            new_node.append(item)
+            last_node = item
+    return new_node
+
+
 flatten = lambda l: [item for sublist in l for item in sublist]
 
 
 def merge_result(result: py2neo.Cursor):
     nodes, relations = list(), list()
     for record in result:
-        print(record)
+        # print(record)
         for n, r in permlize_result(record):
             relations.append(r) if n is None else nodes.append(n)
     return {
         'nodes': nodes,
-        'relationships': relations
+        'relationships': remove_duplicate_relationships(relations)
     }
+
+
 """
 /organization/4296405163
 /person/34418264994
@@ -76,7 +100,7 @@ def merge_result(result: py2neo.Cursor):
 
 
 @app.route('/person/<pid>/organization/<oid>')
-def person_organization(pid,oid):
+def person_organization(pid, oid):
     """
     1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
     为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
