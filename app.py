7d2699d3c4d5ef729ed412050abd5168f47ec4da
mapleFU <1506118561@qq.com>
2019-06-16 11:12:41 +0800

    [Fix] Add label and fix from-to bug

--- app.py
+++ app.py
@@ -21,18 +21,29 @@ g = Graph(driver_address)
 app = Flask(__name__)
 
 
+LABEL_ATTR_SET =set(["institution", "title", "organizationName", "givenName"])
+
+
+def tag_label(value_dict: dict):
+    for k, v in value_dict.items():
+        if k in LABEL_ATTR_SET:
+            value_dict["label"] = value_dict[k]
+            return
+    print("Warning, No label!")
+
+
 def permlize_node_result(record_node: Node):
     value_dict = dict(record_node)
     value_dict["id"] = value_dict['permID']
+    tag_label(value_dict)
     return value_dict
 
-#？？？？？？
+
 def permlize_relationship_result(record_node: Relationship):
     value_dict = dict(record_node)
     value_dict["from"] = dict(record_node.start_node)['permID']
-    value_dict["to"] = dict(record_node.start_node)['permID'] #写错了？所有边都成环
+    value_dict["to"] = dict(record_node.end_node)['permID']
     return value_dict
-#node,还需要添加类别(label)信息
 
 
 def permlize_result(record: py2neo.Record):
@@ -64,14 +75,12 @@ def merge_result(result: py2neo.Cursor):
 """
 
 
-#1.个人-组织
-"""
-1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
-为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
-"""
 @app.route('/person/<pid>/organization/<oid>')
 def person_organization(pid,oid):
-
+    """
+    1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
+    为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
+    """
     cql = f'''
         MATCH (s:Person {{permID: '{pid}' }})-[p:isPositionIN]-(o:Organization{{permID: '{oid}' }})
         return s, p, o
@@ -79,27 +88,27 @@ def person_organization(pid,oid):
     print(cql)
     return jsonify(merge_result(g.run(cql)))
 
-#2.
-"""
-1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
-为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
-"""
+
 @app.route('/person/<pid1>/person/<pid2>')
 def person_person(pid1,pid2):
-
+    """
+    1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
+    为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
+    """
     cql = f'''
         MATCH (s:Person {{permID: '{pid1}' }})-[p]-(o:Person{{permID: '{pid2}' }})
         return s, p, o
     '''
     print(cql)
     return jsonify(merge_result(g.run(cql)))
-#3.
-"""
-1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
-为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
-"""
+
+
 @app.route('/organization/<oid1>/organization/<oid2>')
 def organization_organization(oid1, oid2):
+    """
+    1.输⼊入两个实体（如Alibaba和Tencent），查询其可能存在的多跳关系。其中多跳关系定义
+    为，通过多条边链式的连接在⼀一起。如Alibaba -> (Industry) Internet -> Tencent
+    """
     cql = f'''
         MATCH (s:Organization {{permID: '{oid1}' }})-[p]-(o:Organization{{permID: '{oid2}' }})
         return s, p, o
@@ -107,42 +116,48 @@ def organization_organization(oid1, oid2):
     print(cql)
     return jsonify(merge_result(g.run(cql)))
 
-#4.
-"""
-2。输⼊入⼀一个实体（如Alibaba），查询其关联的所有关系和关联实体；
-"""
+
 @app.route('/person/<pid>')
 def person(pid):
+    """
+    2。输⼊入⼀一个实体（如Alibaba），查询其关联的所有关系和关联实体；
+    """
     cql=f'''MATCH (s:Person{{permID:'{pid}'}})-[p]-(o) return s, p, o '''
     print(cql)
     return jsonify(merge_result(g.run(cql)))
 
-#5.
-"""
-2。输⼊入⼀一个实体（如Alibaba），查询其关联的所有关系和关联实体；
-"""
+
 @app.route('/organization/<oid>')
-def organization(oid):
+def organization_show_all(oid):
+    """
+    #5.
+    2。输⼊入⼀一个实体（如Alibaba），查询其关联的所有关系和关联实体；
+    """
     cql=f'''MATCH (s:Organization{{permID:'{oid}'}})-[p]-(o) return  s, p, o'''
     print(cql)
     return jsonify(merge_result(g.run(cql)))
 
-#6.查看某个institution相关的person
-#不在要求内
+
 @app.route('/institution/<iname>')
 def organization(iid):
+    """
+    #6.查看某个institution相关的person
+    #不在要求内
+    """
     cql=f'''MATCH (s:Institution{{name:'{iname}'}})-[p]-(o) return  s, p, o'''
     print(cql)
     return jsonify(merge_result(g.run(cql)))
 
-#7.查看某个industryGroup相关的organization
-#不在要求内
-@app.route('/idustryGroup/<ilabel>')
-def organization(ilabel):
+
+@app.route('/industryGroup/<ilabel>')
+def industry_group_to_organization(ilabel):
+    """
+    #7.查看某个industryGroup相关的organization
+    #不在要求内
+    """
     cql=f'''MATCH (s:IndustryGroup{{name:'{ilabel}'}})-[p]-(o) return  s, p, o'''
     print(cql)
     return jsonify(merge_result(g.run(cql)))
-
 
 
 if __name__ == '__main__':
